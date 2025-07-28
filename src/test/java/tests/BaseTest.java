8d900f58da5fac863222610ea7521af863d5a493
Anastasiia Ladyzhets <anastasiialadyzhets@Anastasiias-MacBook-Pro.local>
2025-07-28 19:25:09 +0300

    fix remote

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -24,8 +24,6 @@ public class BaseTest {
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
